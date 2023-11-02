ec342e90d579770bd620488cd80fe82e86654670
zouhl <zouhl0924@gmail.com>
2023-11-02 21:14:20 +0800

    mybatis注解

--- MySpringBoot/src/main/java/com/homura/myspringboot/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.homura.myspringboot.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"com.homura.myspringboot"})
-public class AppConfig {
-}
